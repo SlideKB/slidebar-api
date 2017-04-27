8e3ba8684ad9ba016cb98e1175d8fc1805f87dbd
PureSpider <alex.lamprecht1@gmail.com>
2017-04-27 19:10:28 +0200

    Add platform specific annotation and constants

---
+++ src/main/java/com/github/slidekb/api/Platform.java
@@ -0,0 +1,5 @@
+package com.github.slidekb.api;
+
+public enum Platform {
+    win32, win64, osx32, osx64, nix32, nix64
+}
---
+++ src/main/java/com/github/slidekb/api/PlatformSpecific.java
@@ -0,0 +1,5 @@
+package com.github.slidekb.api;
+
+public @interface PlatformSpecific {
+    Platform value();
+}
