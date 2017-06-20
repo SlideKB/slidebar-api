c932f36bf288301b085fc607283cb9daea52867f
PureSpider <alex.lamprecht1@gmail.com>
2017-06-20 23:46:50 +0200

    Some more annotation flavoring

--- src/main/java/com/github/slidekb/api/PluginVersion.java
+++ src/main/java/com/github/slidekb/api/PluginVersion.java
@@ -1,8 +1,14 @@
 package com.github.slidekb.api;
 
+import static java.lang.annotation.ElementType.TYPE;
+
+import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
+@Documented
+@Target(TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface PluginVersion {
     int value();
